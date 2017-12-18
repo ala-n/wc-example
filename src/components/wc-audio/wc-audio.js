763b700c8a76108e02d7b129516ca38f737c0880
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2017-12-18 16:12:16 +0300

    ShDom attached

--- src/components/wc-audio/wc-audio.js
+++ src/components/wc-audio/wc-audio.js
@@ -1,14 +1,31 @@
 
-
+const template = document.createElement('template');
+template.innerHTML = `
+	<style>
+		:host {
+			display: block;
+			text-align: center;
+		}
+		button {
+			display: inline-block;
+			background: transparent;
+			color: white;
+		}
+	</style>
+	<button> Hi! </button>
+`;
 
 class WCAudio extends HTMLElement {
 
 	constructor() {
 		super();
+		this.attachShadow({
+			mode: 'open'
+		});
+		this.shadowRoot.appendChild(template.content.cloneNode(true));
 	}
 
 	connectedCallback() {
-		this.innerHTML = '<button> Hi! </button>';
 	}
 
 	disconnectedCallback() {
